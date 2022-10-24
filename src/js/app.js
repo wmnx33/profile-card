a627a782cee686b7f6687952dbd6fa32b2f32aa5
wmnx33 <wmnxreative@gmail.com>
2022-10-24 23:57:14 +0000

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>Shannon Shupak</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
